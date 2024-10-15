e6dd46b4f81f4d6ecf1a972e63c3a8b4841b71cc
Luky Drum <lukbubi12@gmail.com>
2024-10-15 22:29:16 +0200

    Add oil

--- src/particles/mod.rs
+++ src/particles/mod.rs
@@ -1,9 +1,11 @@
 pub mod constants;
+mod p_oil;
 mod p_rock;
 mod p_sand;
 mod p_water;
 mod particle;
 
+pub use p_oil::Oil;
 pub use p_rock::Rock;
 pub use p_sand::Sand;
 pub use p_water::Water;
---
+++ src/particles/p_oil.rs
@@ -0,0 +1,71 @@
+use std::collections::LinkedList;
+
+use rand::random;
+
+use crate::particles::constants::*;
+use crate::particles::{get_near_color, Particle};
+use crate::Offset;
+
+const COLOR: u32 = 0x00996E17;
+const DENSITY: u8 = 120;
+
+#[derive(Clone)]
+pub struct Oil {
+    velocity: f32,
+    color: u32,
+}
+
+impl Oil {
+    pub fn new() -> Box<dyn Particle> {
+        Box::new(Oil {
+            velocity: DEFAULT_VELOCITY,
+            color: get_near_color(COLOR),
+        })
+    }
+}
+
+impl Particle for Oil {
+    fn get_color(&self) -> u32 {
+        self.color
+    }
+
+    fn get_density(&self) -> u8 {
+        DENSITY
+    }
+
+    fn get_velocity(&self) -> f32 {
+        self.velocity
+    }
+
+    fn get_max_offsets(&self) -> LinkedList<Offset> {
+        let mut lst = LinkedList::new();
+        let vel = self.velocity as i32;
+
+        lst.push_back(Offset::new(0, 1) * vel);
+        if random() {
+            lst.push_back(Offset::new(1, 0) * vel);
+            lst.push_back(Offset::new(-1, 0) * vel);
+        } else {
+            lst.push_back(Offset::new(-1, 0) * vel);
+            lst.push_back(Offset::new(1, 0) * vel);
+        }
+
+        lst
+    }
+
+    fn is_moveable(&self) -> bool {
+        true
+    }
+
+    fn is_solid(&self) -> bool {
+        false
+    }
+
+    fn reset_velocity(&mut self) -> () {
+        self.velocity = DEFAULT_VELOCITY;
+    }
+
+    fn apply_acceleration(&mut self, acc: f32) -> () {
+        self.velocity = (self.velocity + acc).clamp(DEFAULT_VELOCITY, MAX_VELOCITY);
+    }
+}
