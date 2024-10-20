8eaee62a5ee242364ce9ad139e9acff286f1ac4c
Luky Drum <lukbubi12@gmail.com>
2024-10-20 21:34:28 +0200

    Fix burnability enum mistake

--- src/particles/properties.rs
+++ src/particles/properties.rs
@@ -1,9 +1,10 @@
 /// Describes how does the particle burn.
 pub enum Burnability {
     /// The particle is currently on fire, can light up other particles.
-    IsBurning,
-    /// The particle can burn if next to fire, contains a durability parameter (How long before the particle burns down).
-    CanBurn(u8),
+    /// Contains a durability parameter (How long before the particle burns down).
+    IsBurning(u8),
+    /// The particle can burn if next to fire.
+    CanBurn,
     /// The particle does not react to fire in any way.
     None,
 }
