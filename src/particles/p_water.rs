882d5cc5cdf84748684a3d64362711c95d5633e0
Luky Drum <lukbubi12@gmail.com>
2024-10-27 22:33:28 +0100

    Let water handle its movement in update

--- src/particles/p_water.rs
+++ src/particles/p_water.rs
@@ -1,13 +1,13 @@
 use std::collections::LinkedList;
 
-use rand::{random, thread_rng};
+use rand::random;
 
 use crate::particles::constants::*;
 use crate::particles::{get_near_color, Particle};
 use crate::Offset;
 
 // use super::{Burnability, Neighborhood, ParticleChange, Vapor};
-use super::{Burnability, Neighborhood, ParticleChange, Sand};
+use super::{Burnability, Neighborhood, ParticleChange};
 
 const COLOR: u32 = 0xFF326ECF;
 const DENSITY: u8 = 128;
@@ -16,7 +16,7 @@ const DENSITY: u8 = 128;
 pub struct Water {
     velocity: f32,
     color: u32,
-    x_dir: i32,
+    movement: Offset,
 }
 
 impl Water {
@@ -24,7 +24,7 @@ impl Water {
         Box::new(Water {
             velocity: DEFAULT_VELOCITY,
             color: get_near_color(COLOR),
-            x_dir: Self::random_x_dir(),
+            movement: Offset::new(0, 1),
         })
     }
 
@@ -32,16 +32,8 @@ impl Water {
         Box::new(Water {
             velocity: DEFAULT_VELOCITY,
             color: get_near_color(color),
-            x_dir: Self::random_x_dir(),
+            movement: Offset::new(0, 1),
         })
-    }
-
-    fn random_x_dir() -> i32 {
-        if random() {
-            -1
-        } else {
-            1
-        }
     }
 }
 
@@ -63,15 +55,12 @@ impl Particle for Water {
     }
 
     fn _get_offsets(&self) -> LinkedList<Offset> {
+        LinkedList::new()
+    }
+
+    fn get_max_offsets(&self) -> LinkedList<Offset> {
         let mut lst = LinkedList::new();
-        let vel = self.velocity as i32;
-
-        lst.push_back(Offset::new(0, 1) * vel);
-        lst.push_back(Offset::new(self.x_dir, 1) * vel);
-        lst.push_back(Offset::new(-self.x_dir, 1) * vel);
-        lst.push_back(Offset::new(self.x_dir, 0) * vel);
-        lst.push_back(Offset::new(-self.x_dir, 0) * vel);
-
+        lst.push_back(self.movement * (self.velocity as i32));
         lst
     }
 
@@ -96,6 +85,28 @@ impl Particle for Water {
     }
 
     fn update(&self, neigborhood: Neighborhood) -> ParticleChange {
+        let mut new_water = self.clone();
+
+        for_else!(
+            for off in [Offset::new(0, 1), Offset::new(-1, 1), Offset::new(1, 1), Offset::new(-1, 0), Offset::new(1, 0)] => {
+                match neigborhood.on_relative(&off) {
+                    None => {
+                        new_water.movement = off;
+                        break;
+                    }
+                    Some(other) => {
+                        if self.can_switch_with(other) {
+                            new_water.movement = off;
+                            break;
+                        }
+                    }
+                }
+            } else {
+                new_water.movement = Offset::zero();
+                new_water.velocity = DEFAULT_VELOCITY;
+            }
+        );
+
         // Check number of neighbors that are IsBurning and AntiBurn
         let mut count = 0;
         for opt in neigborhood.iter() {
@@ -112,27 +123,7 @@ impl Particle for Water {
             // ParticleChange::Changed(Some(Vapor::new()))
             ParticleChange::None
         } else {
-            // Check left and right for direction change
-            // Left
-            if self.x_dir == -1 {
-                if let Some(p) = &neigborhood.left() {
-                    if p.get_density() >= self.get_density() {
-                        let mut new_water = self.clone();
-                        new_water.x_dir = 1;
-                        return ParticleChange::Changed(Some(Box::new(new_water)));
-                    }
-                }
-            } else if self.x_dir == 1 {
-                if let Some(p) = &neigborhood.right() {
-                    if p.get_density() >= self.get_density() {
-                        let mut new_water = self.clone();
-                        new_water.x_dir = -1;
-                        return ParticleChange::Changed(Some(Box::new(new_water)));
-                    }
-                }
-            }
-
-            ParticleChange::None
+            ParticleChange::Changed(Some(Box::new(new_water)))
         }
     }
 }
